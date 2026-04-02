7ccb302edaab726f4bc91b836c967696f07c0bd8
zis76 <76632633+zis76@users.noreply.github.com>
2026-04-02 12:45:26 -0500

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,19 +1,26 @@
 import { round, score } from './score.js';
 
 /**
- * Path to directory containing `_list.json` and all levels
+ * Path to directory containing list files and levels
  */
 const dir = '/data';
 
+
+// MAIN LIST
+
 export async function fetchList() {
     const listResult = await fetch(`${dir}/_list.json`);
+
     try {
         const list = await listResult.json();
+
         return await Promise.all(
             list.map(async (path, rank) => {
                 const levelResult = await fetch(`${dir}/${path}.json`);
+
                 try {
                     const level = await levelResult.json();
+
                     return [
                         {
                             ...level,
@@ -36,6 +43,47 @@ export async function fetchList() {
     }
 }
 
+// ILL
+
+export async function fetchIllist() {
+    const listResult = await fetch(`${dir}/_illist.json`);
+
+    try {
+        const list = await listResult.json();
+
+        return await Promise.all(
+            list.map(async (path, rank) => {
+                const levelResult = await fetch(`${dir}/${path}.json`);
+
+                try {
+                    const level = await levelResult.json();
+
+                    return [
+                        {
+                            ...level,
+                            path,
+                            records: level.records.sort(
+                                (a, b) => b.percent - a.percent,
+                            ),
+                        },
+                        null,
+                    ];
+                } catch {
+                    console.error(`Failed to load illist level #${rank + 1} ${path}.`);
+                    return [null, path];
+                }
+            }),
+        );
+    } catch {
+        console.error(`Failed to load illist.`);
+        return null;
+    }
+}
+
+
+// EDITORS
+
+
 export async function fetchEditors() {
     try {
         const editorsResults = await fetch(`${dir}/_editors.json`);
@@ -46,28 +94,34 @@ export async function fetchEditors() {
     }
 }
 
+
+// MAIN LEADERBOARD  
+
 export async function fetchLeaderboard() {
     const list = await fetchList();
 
     const scoreMap = {};
     const errs = [];
+
     list.forEach(([level, err], rank) => {
         if (err) {
             errs.push(err);
             return;
         }
 
-        // Verification
-        const verifier = Object.keys(scoreMap).find(
-            (u) => u.toLowerCase() === level.verifier.toLowerCase(),
-        ) || level.verifier;
+        // Verifier
+        const verifier =
+            Object.keys(scoreMap).find(
+                (u) => u.toLowerCase() === level.verifier.toLowerCase(),
+            ) || level.verifier;
+
         scoreMap[verifier] ??= {
             verified: [],
             completed: [],
             progressed: [],
         };
-        const { verified } = scoreMap[verifier];
-        verified.push({
+
+        scoreMap[verifier].verified.push({
             rank: rank + 1,
             level: level.name,
             score: score(rank + 1, 100, level.percentToQualify),
@@ -76,41 +130,40 @@ export async function fetchLeaderboard() {
 
         // Records
         level.records.forEach((record) => {
-            const user = Object.keys(scoreMap).find(
-                (u) => u.toLowerCase() === record.user.toLowerCase(),
-            ) || record.user;
+            const user =
+                Object.keys(scoreMap).find(
+                    (u) => u.toLowerCase() === record.user.toLowerCase(),
+                ) || record.user;
+
             scoreMap[user] ??= {
                 verified: [],
                 completed: [],
                 progressed: [],
             };
-            const { completed, progressed } = scoreMap[user];
+
             if (record.percent === 100) {
-                completed.push({
+                scoreMap[user].completed.push({
                     rank: rank + 1,
                     level: level.name,
                     score: score(rank + 1, 100, level.percentToQualify),
                     link: record.link,
                 });
-                return;
+            } else {
+                scoreMap[user].progressed.push({
+                    rank: rank + 1,
+                    level: level.name,
+                    percent: record.percent,
+                    score: score(rank + 1, record.percent, level.percentToQualify),
+                    link: record.link,
+                });
             }
-
-            progressed.push({
-                rank: rank + 1,
-                level: level.name,
-                percent: record.percent,
-                score: score(rank + 1, record.percent, level.percentToQualify),
-                link: record.link,
-            });
         });
     });
 
-    // Wrap in extra Object containing the user and total score
     const res = Object.entries(scoreMap).map(([user, scores]) => {
-        const { verified, completed, progressed } = scores;
-        const total = [verified, completed, progressed]
+        const total = Object.values(scores)
             .flat()
-            .reduce((prev, cur) => prev + cur.score, 0);
+            .reduce((sum, r) => sum + r.score, 0);
 
         return {
             user,
@@ -119,6 +172,5 @@ export async function fetchLeaderboard() {
         };
     });
 
-    // Sort by total score
     return [res.sort((a, b) => b.total - a.total), errs];
 }
